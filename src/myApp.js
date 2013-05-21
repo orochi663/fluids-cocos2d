bf5177d73b7bd660d6bef038fd967138a8e8eca8
John Sietsma <john.sietsma@gmail.com>
2013-05-21 21:57:21 +1000

    Semi-stripped hello world.

---
+++ src/myApp.js
@@ -0,0 +1,98 @@
+/****************************************************************************
+ Copyright (c) 2010-2012 cocos2d-x.org
+ Copyright (c) 2008-2010 Ricardo Quesada
+ Copyright (c) 2011      Zynga Inc.
+
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+var CircleSprite = cc.Sprite.extend({
+    _degree:0,
+    ctor:function () {
+        this._super();
+    },
+    draw:function () {
+        cc.drawingUtil.setDrawColor4B(255,255,255,255);
+
+        if (this._degree < 0)
+            this._degree = 360;
+        cc.drawingUtil.drawCircle(cc.PointZero(), 30, cc.DEGREES_TO_RADIANS(this._degree), 60, true);
+    },
+    myUpdate:function (dt) {
+        this._degree -= 6;
+    }
+});
+
+var Helloworld = cc.Layer.extend({
+    isMouseDown:false,
+    helloImg:null,
+    helloLabel:null,
+    circle:null,
+    sprite:null,
+
+    init:function () {
+        var selfPointer = this;
+        this._super();
+
+        var size = cc.Director.getInstance().getWinSize();
+
+        this.helloLabel = cc.LabelTTF.create("Hello World", "Arial", 38);
+        this.helloLabel.setPosition(cc.p(size.width / 2, size.height-40));
+        this.addChild(this.helloLabel, 5);
+
+        var lazyLayer = cc.Layer.create();
+        this.addChild(lazyLayer);
+
+        // add "HelloWorld" splash screen"
+        this.sprite = cc.Sprite.create("res/HelloWorld.png");
+        this.sprite.setPosition(cc.p(size.width / 2, size.height / 2));
+
+        lazyLayer.addChild(this.sprite, 0);
+
+        this.setTouchEnabled(true);
+        return true;
+    },
+    onTouchesBegan:function (touches, event) {
+        this.isMouseDown = true;
+    },
+    onTouchesMoved:function (touches, event) {
+        if (this.isMouseDown) {
+            if (touches) {
+                //this.circle.setPosition(cc.p(touches[0].getLocation().x, touches[0].getLocation().y));
+            }
+        }
+    },
+    onTouchesEnded:function (touches, event) {
+        this.isMouseDown = false;
+    },
+    onTouchesCancelled:function (touches, event) {
+        console.log("onTouchesCancelled");
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new Helloworld();
+        layer.init();
+        this.addChild(layer);
+    }
+});
